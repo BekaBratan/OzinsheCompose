005b0c49907ba3d68c9222e21acd583f296199ac
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-02-17 17:15:41 +0500

    Add inspection profiles and update HomeScreen layout with movie cards

--- app/src/main/java/com/example/ozinshecompose/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/ozinshecompose/presentation/HomeScreen.kt
@@ -2,9 +2,11 @@ package com.example.ozinshecompose.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -27,6 +29,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.ozinshecompose.R
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ozinshecompose.ui.theme.Gray400
+import com.example.ozinshecompose.ui.theme.Gray900
+import com.example.ozinshecompose.ui.theme.Pink40
+import com.example.ozinshecompose.ui.theme.Primary500
+import com.example.ozinshecompose.ui.theme.White
 
 @Composable
 fun HomeScreen() {
@@ -42,72 +49,133 @@ fun HomeScreen() {
 
 @Preview
 @Composable
-fun MainMoviesList(){
-    val painter = painterResource(id = R.drawable.ic_launcher_background)
-
-
+fun MoviesList(){
     LazyRow(
-        contentPadding = PaddingValues(horizontal = 24.dp)
+        contentPadding = PaddingValues(horizontal = 24.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         items(10) { index ->
-            Column(
+            MainMovieCard()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainMovieCard() {
+    val painter = painterResource(id = R.drawable.ic_launcher_background)
+
+    Column(
+        modifier = Modifier
+            .width(316.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(164.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = "image",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .width(316.dp)
-                    .padding(end = 16.dp)
+                    .fillMaxSize()
+                    .clip(shape = RoundedCornerShape(12.dp))
+            )
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
             ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(164.dp)
-                ) {
-                    Image(
-                        painter = painter,
-                        contentDescription = "image",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .clip(shape = RoundedCornerShape(12.dp))
-                    )
-                    Box(
-                        modifier = Modifier
-                            .padding(8.dp)
-                    ) {
-                        Text(
-                            text = "Телехикая",
-                            textAlign = TextAlign.Center,
-                            fontFamily = fontFamily,
-                            fontSize = 12.sp,
-                            maxLines = 1,
-                            fontWeight = FontWeight(500),
-                            color = Color.White,
-                            modifier = Modifier
-                                .clip(shape = RoundedCornerShape(8.dp))
-                                .background(Color(126, 45, 252, 255))
-                                .padding(8.dp, 4.dp)
-
-                        )
-                    }
-
-                }
-
                 Text(
-                    text = "Қызғалдақтар мекені",
-                    fontFamily = fontFamily,
-                    fontSize = 14.sp,
-                    maxLines = 1,
-                    fontWeight = FontWeight(700),
-                    color = Color.White
-                )
-                Text(
-                    text = "Шытырман оқиғалы мультсериал Елбасының «Ұлы даланың жеті қыры» бағдарламасы аясында жүздеген өнімдер шығарылған.",
+                    text = "Телехикая",
+                    textAlign = TextAlign.Center,
                     fontFamily = fontFamily,
                     fontSize = 12.sp,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                    fontWeight = FontWeight(400),
-                    color = Color(156, 163, 175, 255)
+                    maxLines = 1,
+                    fontWeight = FontWeight(500),
+                    color = White,
+                    modifier = Modifier
+                        .clip(shape = RoundedCornerShape(8.dp))
+                        .background(Primary500)
+                        .padding(8.dp, 4.dp)
+
                 )
             }
+
         }
+
+        Spacer(Modifier.size(16.dp))
+
+        Text(
+            text = "Қызғалдақтар мекені",
+            fontFamily = fontFamily,
+            fontSize = 14.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight(700),
+            color = Gray900
+        )
+
+        Spacer(Modifier.size(8.dp))
+
+        Text(
+            text = "Шытырман оқиғалы мультсериал Елбасының «Ұлы даланың жеті қыры» бағдарламасы аясында жүздеген өнімдер шығарылған.",
+            fontFamily = fontFamily,
+            fontSize = 12.sp,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight(400),
+            color = Gray400
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PosterMovieCard() {
+    val painter = painterResource(id = R.drawable.ic_launcher_background)
+
+    Column(
+        modifier = Modifier
+            .width(112.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(164.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = "image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(shape = RoundedCornerShape(8.dp))
+            )
+        }
+
+        Spacer(Modifier.size(8.dp))
+
+        Text(
+            text = "Қызғалдақтар мекені",
+            fontFamily = fontFamily,
+            fontSize = 12.sp,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight(600),
+            color = Gray900
+        )
+
+        Spacer(Modifier.size(4.dp))
+
+        Text(
+            text = "Шытырман оқиғалы мультсериал Елбасының «Ұлы даланың жеті қыры» бағдарламасы аясында жүздеген өнімдер шығарылған.",
+            fontFamily = fontFamily,
+            fontSize = 12.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight(400),
+            color = Gray400
+        )
     }
 }
--- app/src/main/java/com/example/ozinshecompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/ozinshecompose/ui/theme/Color.kt
@@ -1,6 +1,8 @@
 package com.example.ozinshecompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.example.ozinshecompose.R
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +11,30 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Gray50 = Color(0xFFF9FAFB)
+val Gray100 = Color(0xFFF3F4F6)
+val Gray200 = Color(0xFFE5E7EB)
+val Gray300 = Color(0xFFD1D5DB)
+val Gray400 = Color(0xFF9CA3AF)
+val Gray500 = Color(0xFF6B7280)
+val Gray600 = Color(0xFF4B5563)
+val Gray700 = Color(0xFF374151)
+val Gray800 = Color(0xFF1F2937)
+val Gray900 = Color(0xFF111827)
+
+val Primary50 = Color(0xFFF8EEFF)
+val Primary100 = Color(0xFFEAD1FD)
+val Primary200 = Color(0xFFD1A4FC)
+val Primary300 = Color(0xFFB376F7)
+val Primary400 = Color(0xFF9753F0)
+val Primary500 = Color(0xFF6D1EE7)
+val Primary600 = Color(0xFF5415C6)
+val Primary700 = Color(0xFF3E0FA6)
+val Primary800 = Color(0xFF2B0985)
+val Primary900 = Color(0xFF1E056E)
+
+val White = Color(0xFFFFFFFF)
+val Yellow = Color(0xFF00BF71)
+val Green = Color(0xFFFBBC05)
+val Blue = Color(0xFF2370FF)
